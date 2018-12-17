3153e8ede0406c9e1a07940e9d1e85f7f80a6b92
noahkalyesubula <noahtech18@gmail.com>
2018-12-17 12:45:40 -0800

    Integrate all UI pages

--- js/login.js
+++ js/login.js
@@ -8,3 +8,25 @@ function swap(referTo){//referTo refers to the current div where this function i
         referTo.parentNode.classList.add("signup");//adds the signup form
     }
 }
+
+function loginUser(){
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+        if(email == "" || password ==""){
+            alert("Please all enter credentials")
+        }
+        else{
+             if(email == "admin@admin.com" && password=="admin1234"){
+           window.location.href= "admin_control_panel.html";
+          
+        }
+            else if(email == "user@user.com" && password=="user1234"){
+            window.location.href="manage_incidents.html";
+           
+        }
+            else{
+                alert("Wrong credentials");
+         }
+        }
+       
+    }
